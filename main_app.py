1691ea5cc241aa0511435064d53ca7b96a0429f6
Ahmedramadan1997 <167720662+Ahmedramadan1997@users.noreply.github.com>
2025-01-01 21:19:35 +0200

    main_app.py

--- main_app.py
+++ main_app.py
@@ -14,11 +14,11 @@ class LogTransformer(BaseEstimator, TransformerMixin):
     # transformer
     def transform(self, X, y=None):
         assert self.n_features_in == X.shape[1]
-        return np.log(X + 1e-9)
-    
-setattr(__name__, 'LogTransformer', LogTransformer)
+        return np.log(X + 1e-9)  # إضافة قيمة صغيرة لتجنب log(0)
+
 app = Flask(__name__)
 
+# تحميل النموذج المدرب
 model = joblib.load("oversampling_pipeline.pkl")
 
 @app.route('/')
@@ -27,25 +27,27 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    gender = request.form['gender']
-    age = int(request.form['age'])
-    location = request.form['location']
-    hypertension = int(request.form['hypertension'])
-    heart_disease = int(request.form['heart_disease'])
-    smoking_history = request.form['smoking_history']
-    bmi = float(request.form['bmi'])
-    hbA1c_level = float(request.form['hbA1c_level'])
-    blood_glucose_level = int(request.form['blood_glucose_level'])
-    input_data = pd.DataFrame([[gender, age, location, hypertension, heart_disease, smoking_history, bmi, hbA1c_level, blood_glucose_level]], columns = ["gender", "age", "location", "hypertension", "heart_disease", "smoking_history", "bmi", "hbA1c_level", "blood_glucose_level"])
-    print(type(age))
-    print(input_data)
+    try:
+        gender = request.form['gender']
+        age = int(request.form['age'])
+        location = request.form['location']
+        hypertension = int(request.form['hypertension'])
+        heart_disease = int(request.form['heart_disease'])
+        smoking_history = request.form['smoking_history']
+        bmi = float(request.form['bmi'])
+        hbA1c_level = float(request.form['hbA1c_level'])
+        blood_glucose_level = int(request.form['blood_glucose_level'])
+        
+        input_data = pd.DataFrame([[gender, age, location, hypertension, heart_disease, smoking_history, bmi, hbA1c_level, blood_glucose_level]], 
+                                   columns=["gender", "age", "location", "hypertension", "heart_disease", "smoking_history", "bmi", "hbA1c_level", "blood_glucose_level"])
+        
+        prediction = model.predict(input_data)
+        prediction_text = 'Patient has Diabetes' if prediction[0] == 1 else 'Patient does not have Diabetes'
+        
+        return render_template('index.html', prediction_text=prediction_text)
     
-
-    prediction = model.predict(input_data)
-    print(prediction)
-    prediction_text = 'Patient has Diabetes' if prediction[0] == 1 else 'Patient does not have Diabetes'
-    
-    return render_template('index.html', prediction_text=prediction_text)
+    except Exception as e:
+        return render_template('index.html', prediction_text='Error: ' + str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
